99b8bf0765396845eccfaf7457e8427f7a2f1fc8
Personal <Personal@Familia>
2021-03-01 17:19:50 -0600

    NumLetras59538

---
+++ src/operadorespromedio59538/OperadoresPromedio59538.java
@@ -0,0 +1,35 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package operadorespromedio59538;
+
+/**
+ *
+ * @author JossmarOswaldoKuCupul
+ */
+public class OperadoresPromedio59538 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Valores
+        int valor1 = 6;
+        float valor2 = 3.5F;
+        int valor3 = 5;
+        float valor4 = 4.5F;
+        int valor5 = 7;
+        
+        // Imprimir valores y promedio
+        System.out.println("valor1 = " + valor1);
+        System.out.println("valor2 = " + valor2);
+        System.out.println("valor3 = " + valor3);
+        System.out.println("valor4 = " + valor4);
+        System.out.println("valor5 = " + valor5);
+        System.out.println("promedioValores = " + (valor1 + valor2 + valor3 + valor4 + valor5) / 5);
+    }
+    
+}
